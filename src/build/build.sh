1dd6beb451935afdaf911b0626b9b4fe9d3dfc0d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 00:06:19 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+puppet apply /tmp/build/etc/puppet/manifests/build.pp
+
+cp /tmp/build/etc/puppet/manifests/run.pp /etc/puppet/manifests/run.pp
+
--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+puppet apply /opt/build/etc/puppet/manifests/run.pp
