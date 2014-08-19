79bd5690c93d4dab885f178543e346ca9af84814
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 07:32:52 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 puppet apply /tmp/build/etc/puppet/manifests/build.pp
-
-cp /tmp/build/etc/puppet/manifests/run.pp /etc/puppet/manifests/run.pp
--- src/build/etc/puppet/manifests/build.pp
+++ src/build/etc/puppet/manifests/build.pp
@@ -1,3 +1,6 @@
 node default {
-
+  file { '/etc/puppet/manifests/run.pp':
+    ensure => present,
+    source => '/tmp/build/etc/puppet/manifests/run.pp',
+  }
 }
