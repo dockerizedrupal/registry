00ac92cbec8f23c300372523a9ba8bc61a75520d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 23:17:03 +0200

    Update

--- src/run/modules/registry/manifests/nginx.pp
+++ src/run/modules/registry/manifests/nginx.pp
@@ -1,7 +1,7 @@
 class registry::nginx {
   file { '/etc/nginx/conf.d/registry.conf':
     ensure => present,
-    source => 'puppet:///modules/registryetc/nginx/conf.d/registry.conf',
+    source => 'puppet:///modules/registry/etc/nginx/conf.d/registry.conf',
     mode => 644
   }
 
