cdeff2a76b59ee4f6c9ae5ab2f0700a108f02316
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-13 19:03:11 +0200

    Update

--- src/run/modules/registry/manifests/nginx.pp
+++ src/run/modules/registry/manifests/nginx.pp
@@ -1,16 +1,4 @@
 class registry::nginx {
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => present,
-    content => template('registry/default.conf.erb'),
-    mode => 644
-  }
-
-  file { '/etc/nginx/conf.d/default-ssl.conf':
-    ensure => present,
-    content => template('registry/default-ssl.conf.erb'),
-    mode => 644
-  }
-
   exec { "htpasswd -b -c /etc/nginx/registry.htpasswd '$username' '$password'":
     timeout => 0,
     path => ['/usr/bin']
