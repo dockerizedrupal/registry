343fdaf3820a3a8734d836ea3a8d4534382a020d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-13 18:47:44 +0200

    Update

--- src/run/modules/registry/manifests/nginx.pp
+++ src/run/modules/registry/manifests/nginx.pp
@@ -1,7 +1,13 @@
 class registry::nginx {
-  file { '/etc/nginx/conf.d/registry.conf':
+  file { '/etc/nginx/conf.d/default.conf':
     ensure => present,
-    content => template('registry/registry.conf.erb'),
+    content => template('registry/default.conf.erb'),
+    mode => 644
+  }
+
+  file { '/etc/nginx/conf.d/default-ssl.conf':
+    ensure => present,
+    content => template('registry/default-ssl.conf.erb'),
     mode => 644
   }
 
