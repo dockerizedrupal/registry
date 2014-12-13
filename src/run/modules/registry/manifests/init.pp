ddda9f5e967b903f47a5e4500adc192b5505c6a0
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-13 18:33:32 +0200

    Update

--- src/run/modules/registry/manifests/init.pp
+++ src/run/modules/registry/manifests/init.pp
@@ -1,4 +1,6 @@
 class registry {
+  require registry::nginx
+
   file { '/usr/local/lib/python2.7/dist-packages/config/config.yml':
     ensure => present,
     source => 'puppet:///modules/registry/usr/local/lib/python2.7/dist-packages/config/config.yml',
