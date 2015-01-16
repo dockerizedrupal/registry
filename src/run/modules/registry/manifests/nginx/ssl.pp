071d1de9d37f26599e02d34f2fc954dc18fe11d6
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-17 01:51:55 +0200

    Update

--- src/run/modules/registry/manifests/nginx/ssl.pp
+++ src/run/modules/registry/manifests/nginx/ssl.pp
@@ -10,7 +10,7 @@ class registry::nginx::ssl {
     require => File['/root/opensslCA.cnf']
   }
 
-  exec { "openssl req -x509 -new -nodes -key /registry/ssl/private/registryCA.key -days 365 -config /root/opensslCA.cnf -out /registry/ssl/certs/registryCA.crt":
+  exec { "openssl req -sha256 -x509 -new -days 3650 -extensions v3_ca -key /registry/ssl/private/registryCA.key -config /root/opensslCA.cnf -out /registry/ssl/certs/registryCA.crt":
     timeout => 0,
     path => ['/usr/bin'],
     require => Exec['openssl genrsa -out /registry/ssl/private/registryCA.key 4096']
@@ -19,7 +19,7 @@ class registry::nginx::ssl {
   exec { 'openssl genrsa -out /registry/ssl/private/registry.key 4096':
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec["openssl req -x509 -new -nodes -key /registry/ssl/private/registryCA.key -days 365 -config /root/opensslCA.cnf -out /registry/ssl/certs/registryCA.crt"]
+    require => Exec["openssl req -sha256 -x509 -new -days 3650 -extensions v3_ca -key /registry/ssl/private/registryCA.key -config /root/opensslCA.cnf -out /registry/ssl/certs/registryCA.crt"]
   }
 
   file { '/root/openssl.cnf':
@@ -34,7 +34,7 @@ class registry::nginx::ssl {
     require => File['/root/openssl.cnf']
   }
 
-  exec { "openssl req -x509 -in /registry/ssl/certs/registry.csr -CA /registry/ssl/certs/registryCA.crt -CAkey /registry/ssl/private/registryCA.key -CAcreateserial -out /registry/ssl/certs/registry.crt -days 365":
+  exec { "openssl x509 -req -sha256 -in /registry/ssl/certs/registry.csr -CA /registry/ssl/certs/registryCA.crt -CAkey /registry/ssl/private/registryCA.key -CAcreateserial -config /root/opensslCA.cnf -out /registry/ssl/certs/registry.crt":
     timeout => 0,
     path => ['/usr/bin'],
     require => Exec["openssl req -sha256 -new -key /registry/ssl/private/registry.key -config /root/openssl.cnf -out /registry/ssl/certs/registry.csr"]
