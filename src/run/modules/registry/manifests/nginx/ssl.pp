8636c831bc53a1b2cc63cbdbd0cdccd851d3b495
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-16 20:15:17 +0200

    Update

--- src/run/modules/registry/manifests/nginx/ssl.pp
+++ src/run/modules/registry/manifests/nginx/ssl.pp
@@ -1,10 +1,16 @@
 class registry::nginx::ssl {
-  exec { 'openssl genrsa -out /registry/ssl/private/registryCA.key 4096':
-    timeout => 0,
-    path => ['/usr/bin']
+  file { '/root/opensslCA.cnf':
+    ensure => present,
+    content => template('registry/opensslCA.cnf.erb')
   }
 
-  exec { "openssl req -x509 -new -nodes -key /registry/ssl/private/registryCA.key -days 365 -subj /C=/ST=/L=/O=/CN=registry -out /registry/ssl/certs/registryCA.crt":
+  exec { 'openssl genrsa -out /registry/ssl/private/registryCA.key 4096':
+    timeout => 0,
+    path => ['/usr/bin'],
+    require => File['/root/opensslCA.cnf']
+  }
+
+  exec { "openssl req -x509 -new -nodes -key /registry/ssl/private/registryCA.key -days 365 -config /root/opensslCA.cnf -out /registry/ssl/certs/registryCA.crt":
     timeout => 0,
     path => ['/usr/bin'],
     require => Exec['openssl genrsa -out /registry/ssl/private/registryCA.key 4096']
@@ -13,20 +19,24 @@ class registry::nginx::ssl {
   exec { 'openssl genrsa -out /registry/ssl/private/registry.key 4096':
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec["openssl req -x509 -new -nodes -key /registry/ssl/private/registryCA.key -days 365 -subj /C=/ST=/L=/O=/CN=registry -out /registry/ssl/certs/registryCA.crt"]
+    require => Exec["openssl req -x509 -new -nodes -key /registry/ssl/private/registryCA.key -days 365 -config /root/opensslCA.cnf -out /registry/ssl/certs/registryCA.crt"]
   }
 
-  $subj = "/C=/ST=/L=/O=/CN=$server_name"
+  file { '/root/openssl.cnf':
+    ensure => present,
+    content => template('registry/openssl.cnf.erb'),
+    require => Exec['openssl genrsa -out /registry/ssl/private/registry.key 4096']
+  }
 
-  exec { "openssl req -sha256 -new -key /registry/ssl/private/registry.key -subj $subj -out /registry/ssl/certs/registry.csr":
+  exec { "openssl req -sha256 -new -key /registry/ssl/private/registry.key -config /root/openssl.cnf -out /registry/ssl/certs/registry.csr":
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec['openssl genrsa -out /registry/ssl/private/registry.key 4096']
+    require => File['/root/openssl.cnf']
   }
 
   exec { "openssl x509 -req -in /registry/ssl/certs/registry.csr -CA /registry/ssl/certs/registryCA.crt -CAkey /registry/ssl/private/registryCA.key -CAcreateserial -out /registry/ssl/certs/registry.crt -days 365":
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec["openssl req -sha256 -new -key /registry/ssl/private/registry.key -subj $subj -out /registry/ssl/certs/registry.csr"]
+    require => Exec["openssl req -sha256 -new -key /registry/ssl/private/registry.key -config /root/openssl.cnf -out /registry/ssl/certs/registry.csr"]
   }
 }
