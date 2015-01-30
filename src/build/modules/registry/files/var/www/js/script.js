463e21f92cfc0f6933bb17dd56458f95ec83aec9
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-30 22:58:28 +0200

    Update

--- src/build/modules/registry/files/var/www/js/script.js
+++ src/build/modules/registry/files/var/www/js/script.js
@@ -1,7 +1,22 @@
 (function($) {
+  function convert_timestamp_to_local_time() {
+    $('.tag-last-updated').each(function() {
+      var timestamp = $(this).html();
+
+      console.log(timestamp);
+      var time = new Date(timestamp * 1000);
+
+      $(this).html(time);
+    });
+  }
+
   $(function() {
     setInterval(function() {
-      $('#page').load(location.href + ' #page > *');
+      $('#page').load(location.href + ' #page > *', function() {
+        convert_timestamp_to_local_time();
+      });
     }, 1000);
+
+    convert_timestamp_to_local_time();
   });
 })(jQuery);
