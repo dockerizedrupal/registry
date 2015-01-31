6fa6cac06082e6ecf44009a2c73dbb064a044473
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-31 22:33:46 +0200

    Update

--- src/build/modules/registry/files/var/www/js/script.js
+++ src/build/modules/registry/files/var/www/js/script.js
@@ -1,22 +1,27 @@
 (function($) {
-  function convert_timestamp_to_local_time() {
+  function convert_timestamp_to_datetime() {
+    function format_datetime(timestamp) {
+      function pad(s) {
+        return (s < 10) ? '0' + s : s;
+      }
+
+      var d = new Date(timestamp * 1000);
+
+      return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('.') + ' ' + [d.getHours(), d.getMinutes(), d.getSeconds()].join(':');
+    }
+
     $('.tag-last-updated').each(function() {
-      var timestamp = $(this).html();
-
-      console.log(timestamp);
-      var time = new Date(timestamp * 1000);
-
-      $(this).html(time);
+      $(this).html(format_datetime($(this).html()));
     });
   }
 
   $(function() {
     setInterval(function() {
       $('#page').load(location.href + ' #page > *', function() {
-        convert_timestamp_to_local_time();
+        convert_timestamp_to_datetime();
       });
     }, 1000);
 
-    convert_timestamp_to_local_time();
+    convert_timestamp_to_datetime();
   });
 })(jQuery);
